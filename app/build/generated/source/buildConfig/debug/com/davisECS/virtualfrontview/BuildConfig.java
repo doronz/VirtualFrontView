163e53098e6caba6741658ae924b7143a7864f28
Doron <devdoron@gmail.com>
2015-01-28 18:59:51 -0800

    Updated gradle files / SDK files etc. to work with new Android Studio and SDKs.

--- app/build/generated/source/buildConfig/debug/com/davisECS/virtualfrontview/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/davisECS/virtualfrontview/BuildConfig.java
@@ -5,7 +5,7 @@ package com.davisECS.virtualfrontview;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.davisECS.virtualfrontview";
+  public static final String APPLICATION_ID = "com.davisECS.virtualfrontview";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
